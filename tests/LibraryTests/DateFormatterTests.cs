36a08a4b31110fdc962b8121dfbdda11ca200b46
Joacoaires <jose.aires@correo.ucu.edu.uy>
2025-04-30 22:37:55 -0300

    commit de esto

---
+++ tests/LibraryTests/DateFormatterTests.cs
@@ -0,0 +1,30 @@
+﻿using Ucu.Poo.TestDateFormat;
+using NUnit.Framework;
+namespace LibraryTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestFormatoCorrecto()
+    {
+        string resultado = DateFormatter.ChangeFormat("23/08/2005");
+        Assert.AreEqual("2005-08-23",resultado);
+    }
+    
+    [Test]
+    public void TestFormatoIncorrecto()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void TestFechaEnBlanco()
+    {
+        Assert.Pass();
+    }
+}
